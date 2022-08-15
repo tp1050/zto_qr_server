9c99ff35234206a23402791a366203bcd59d4d37
tp1050 <tp1050@gmail.com>
2022-08-15 13:14:51 +0430

    Added logging

--- dool.py
+++ dool.py
@@ -5,12 +5,17 @@
 
 from flask import Flask
 import logging
+from datetime import datetime
 app = Flask(__name__)
+now=datetime.now
 
 
 
 from routes import *
 
 if __name__=="__main__":
-    logging.basicConfig(filename='dool.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(filename='dool.log', level=logging.DEBUG)
+
+    logging.info(f"start: {now()}\n")
     app.run()
+    logging.info(f"end:{now()}")
--- routes.py
+++ routes.py
@@ -3,8 +3,9 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import json
+import logging
 
-from dool import app
+from dool import app,now
 
 @app.route("/")
 def hello_world():
@@ -14,7 +15,8 @@ def hello_world():
 def echoJSON():
     if request.method=='POST':
         data = request.json
-        data['Quantity']=data['Quantity']+66
+        logging.info(f"{now()}:{data}")
         return jsonify(data)
     else:
         return jsonify(json.loads('{"dool": "dool"}'))
+        logging.info("Kos sher zadan")
